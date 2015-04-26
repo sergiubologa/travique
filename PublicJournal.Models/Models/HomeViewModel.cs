9369ccaf713fd13ca9e59cb8a4a0af9e847483fb
Emanuel Marcus <emarcus@pitechnologies.ro>
2015-04-26 11:29:15 +0300

    updated home page dynamically information

---
+++ PublicJournal.Models/Models/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicJournal.Models.Models
+{
+    public class HomeViewModel
+    {
+        public List<CategoryModel> ListCategories { get; set; }
+
+    }
+}
